3d259610c44583bbc7000f434f800b89ee50541c
ammarelshamy <ammarelshamy@gmail.com>
2023-06-22 01:37:44 +0300

    feat: http methods

---
+++ TrendingRepos/Core/HttpMethods.swift
@@ -0,0 +1,16 @@
+//
+//  HttpMethods.swift
+//  TrendingRepos
+//
+//  Created by Ammar Elshamy on 22/06/2023.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case GET
+    case POST
+    case PUT
+    case DELETE
+    case PATCH
+}
