c2287cfef77a99098773bce313cb93128293a362
ammarelshamy <ammarelshamy@gmail.com>
2023-06-21 23:54:13 +0300

    feat: added TrendingRepositories entities

---
+++ TrendingRepos/TrendingRepository.swift
@@ -0,0 +1,43 @@
+//
+//  TrendingRepository.swift
+//  TrendingRepos
+//
+//  Created by Ammar Elshamy on 21/06/2023.
+//
+
+import Foundation
+
+struct TrendingRepositoriesResponse: Codable {
+    let repositories: [TrendingRepository]
+    
+    enum CodingKeys: String, CodingKey {
+        case repositories = "items"
+    }
+}
+
+struct TrendingRepository: Codable {
+    let id: Int
+    let title: String?
+    let owner: RepositoryOwner?
+    let description: String?
+    let language: String?
+    let starsCount: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, owner, description, language
+        case title = "name"
+        case starsCount = "stargazersCount"
+    }
+}
+
+struct RepositoryOwner: Codable {
+    let id: Int
+    let name: String?
+    let avatarURL: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name = "login"
+        case avatarURL = "avatarUrl"
+    }
+}
