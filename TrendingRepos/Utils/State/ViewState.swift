bd7e6ea4731f34e52e3206805b93be7712e0c5ef
ammarelshamy <ammarelshamy@gmail.com>
2023-06-21 12:54:04 +0300

    feat: added ViewState and ViewModelState enums

---
+++ TrendingRepos/Utils/State/ViewState.swift
@@ -0,0 +1,16 @@
+//
+//  ViewState.swift
+//  TrendingRepos
+//
+//  Created by Ammar Elshamy on 21/06/2023.
+//
+
+import Foundation
+
+// We may add empty state if we need it
+
+enum StateUIModel: Equatable {
+    case loading
+    case success
+    case failure
+}
